17538b1b6ecd8b59d25f0b0ce1e1d8c486b2c939
PJ <j.phongsin@gmail.com>
2025-03-11 16:10:51 +1100

    First commit

---
+++ Week1/app.js
@@ -0,0 +1,16 @@
+var async = function(){
+  setTimeout(function () { log("I am coming out although I have been called before the next one")}, 2000)
+}
+
+var adder = function(first, second){
+  var sum = first + second
+  return sum
+}
+
+var log = function(msg){
+  console.log("[Log]: ", msg)
+}
+
+log("The sum is " + adder(5,6))
+async();
+log("This is going to come out before the previous one")
